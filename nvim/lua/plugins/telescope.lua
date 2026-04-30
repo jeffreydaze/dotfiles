a559cca53d3662a9030a78d2d68c64528cb6dff6
jeffreydaze <jeffrey.daze@gmail.com>
2026-04-29 21:48:08 -0500

    Playing with some telescope features

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,4 +3,7 @@ return {
 	dependencies = {
 		'nvim-lua/plenary.nvim'
 	}
+
+	'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
+
 }
