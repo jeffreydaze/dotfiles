56715b191bb9bfa9324137465637d7ee20f8d778
Jeff Daze <jdaze@equitybank.com>
2026-04-30 08:43:03 -0500

    Commenting out the fzf-native until it is worked out.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,6 @@ return {
 		'nvim-lua/plenary.nvim'
 	}
 
-	'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
+--	'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
 
 }
