00e37e61d4567b3a50f3ee4a875a35603445bc55
Jeff Daze <jdaze@equitybank.com>
2026-05-01 10:36:08 -0500

    adding starship config

--- Powershell/pwshprofile.ps1
+++ Powershell/pwshprofile.ps1
@@ -4,7 +4,11 @@ This is the start of consolidating my powershell profile in one place. This prof
 #>
 
 # oh-my-posh theme
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/tokyonight_storm.omp.json' | Invoke-Expression
+#oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/tokyonight_storm.omp.json' | Invoke-Expression
+
+# Starship config
+$env:STARSHIP_CONFIG = "$HOME\dotfiles\starship\starship.toml"
+Invoke-Expression (&starship init powershell)
 
 # Modules Neeed at runtime
 Import-Module -Name Terminal-Icons
