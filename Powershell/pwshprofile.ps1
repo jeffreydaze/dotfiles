0a68f6b0ca3f379c5486163bacd645e66eca7310
Jeff Daze <jdaze@equitybank.com>
2026-09-03 15:38:04 -0500

    PSReadline configurations

--- Powershell/pwshprofile.ps1
+++ Powershell/pwshprofile.ps1
@@ -12,7 +12,36 @@ Invoke-Expression (&starship init powershell)
 
 # Modules Neeed at runtime
 Import-Module -Name Terminal-Icons
-Import-Module PSReadline
+Import-Module -Name PSReadline
+
+# PSReadline Config
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+# PSReadline Theme
+# --- Rosé Pine ANSI helper ---
+function global:RosePine([int]$r, [int]$g, [int]$b) {
+    "$([char]0x1b)[38;2;${r};${g};${b}m"
+}
+
+Set-PSReadLineOption -Colors @{
+    Comment            = RosePine 110 106 134   # muted   #6e6a86
+    Keyword            = RosePine 49  116 143   # pine    #31748f
+    String             = RosePine 246 193 119   # gold    #f6c177
+    Operator           = RosePine 144 140 170   # subtle  #908caa
+    Variable           = RosePine 156 207 216   # foam    #9ccfd8
+    Command            = RosePine 196 167 231   # iris    #c4a7e7
+    Parameter          = RosePine 235 188 186   # rose    #ebbcba
+    Type               = RosePine 156 207 216   # foam    #9ccfd8
+    Number             = RosePine 235 111 146   # love    #eb6f92
+    Member             = RosePine 224 222 244   # text    #e0def4
+    Default            = RosePine 224 222 244   # text    #e0def4
+    Emphasis           = RosePine 235 111 146   # love    #eb6f92
+    Error              = RosePine 235 111 146   # love    #eb6f92
+    Selection          = "$([char]0x1b)[48;2;64;61;82m"   # highlight med bg #403d52
+    InlinePrediction   = RosePine 110 106 134   # muted   #6e6a86
+}
 
 # Importing Zoxide configuration
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
